3898f2f458319de4fc8fa2b9c01030e0ce4a6fc6
Trevor Sherin <trevor.shrn@gmail.com>
2018-10-21 21:40:31 +0100

    Added to movement script and moved to script folder1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 10;
+    private Rigidbody rig;
+
+	void Start ()
+    {
+        rig = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * speed * Time.deltaTime;
+        print (rig);
+        rig.MovePosition(transform.position + movement);
+    }
+}
