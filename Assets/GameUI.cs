2548299301b66bf6cfb9c6adfd8d9fa41e7c78bf
Trevor Sherin <trevor.shrn@gmail.com>
2019-02-20 23:56:45 +0000

    Added tower placement functionality

---
+++ Assets/GameUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+
+    public enum State
+    {
+        Normal,
+        Building
+    }
+
+    public State state { get; private set; }
+    public GameObject towerMarker;
+    PlaceTower buildButton;
+
+    void SetState(State newState)
+    {
+        if (state == newState)
+        {
+            return;
+        }
+        state = newState;
+    }
+
+    public void SetToBuildMode()
+    {
+        if (state != State.Building)
+        {
+            SetState(State.Building);
+            CreateTowerMarker();
+        }
+    }
+
+    void CreateTowerMarker()
+    {
+        Instantiate(towerMarker);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        buildButton = GameObject.Find("BuildTowerButton").GetComponent<PlaceTower>();
+        if (buildButton.IsBuilding())
+        {
+            SetToBuildMode();
+        }
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,21 +3,45 @@
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float speed = 10;
+    public float moveSpeed = 10;
     private Rigidbody rig;
 
-	void Start ()
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    private Camera mainCamera;
+
+    void Start ()
     {
         rig = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
 	}
 	
 	void Update ()
     {
-        float hAxis = Input.GetAxis("Horizontal");
+        moveInput = new Vector3(-Input.GetAxisRaw("Vertical"), 0f, Input.GetAxisRaw("Horizontal"));
+        moveVelocity = moveInput * moveSpeed;
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            //Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+            transform.LookAt(pointToLook);
+        }
+        
+        /*float hAxis = Input.GetAxis("Horizontal");
         float vAxis = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(hAxis, 0, vAxis) * speed * Time.deltaTime;
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * moveSpeed * Time.deltaTime;
         print (rig);
-        rig.MovePosition(transform.position + movement);
+        rig.MovePosition(transform.position + movement);*/
+    }
+
+    void FixedUpdate()
+    {
+        rig.velocity = moveVelocity;
     }
 }
